1a19eb3f1089ce3ec4e08c8d87b5c7d57bd8c72c
FxWolfxe <ndmuench@comcast.net>
2020-03-24 07:24:43 -0400

    initial commit

---
+++ Source/PMSkaven/PMSkaven/PMModInit.cs
@@ -0,0 +1,16 @@
+﻿// PMModInit.cs created by Iron Wolf for PMSkaven on 03/24/2020 7:21 AM
+// last updated 03/24/2020  7:21 AM
+
+using Verse;
+
+namespace PMSkaven
+{
+    [StaticConstructorOnStartup]
+    public static class PMModInit
+    {
+        static PMModInit()
+        {
+            //empty 
+        }
+    }
+}
---
+++ Source/PMSkaven/PMSkaven/PMSkavenMod.cs
@@ -0,0 +1,14 @@
+﻿// PMSkavenMod.cs created by Iron Wolf for PMSkaven on 03/24/2020 7:21 AM
+// last updated 03/24/2020  7:21 AM
+
+using Verse;
+
+namespace PMSkaven
+{
+    public class PMSkavenMod : Mod 
+    {
+        public PMSkavenMod(ModContentPack content) : base(content)
+        {
+        }
+    }
+}
